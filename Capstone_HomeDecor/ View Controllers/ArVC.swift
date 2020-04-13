90b1aa51d31a5136e31e8428e7299ba14da59f64
Parth Dalwadi <pbdalwadi@gmail.com>
2020-04-13 14:54:03 -0400

    - controls added in AR screen

--- Capstone_HomeDecor/ View Controllers/ArVC.swift
+++ Capstone_HomeDecor/ View Controllers/ArVC.swift
@@ -11,22 +11,64 @@ import ARKit
 
 class ArVC: UIViewController {
     
+    @IBOutlet weak var rotateBtn: UIButton!
+    @IBOutlet weak var upBtn: UIButton!
+    @IBOutlet weak var downBtn: UIButton!
+    @IBOutlet weak var leftBtn: UIButton!
+    @IBOutlet weak var rightBtn: UIButton!
     
+    @IBOutlet weak var scaleTF: UITextField!
     
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     var nodeName:String?
+    
+    var CurrentNode: SCNNode?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        
+        
+        // tap gesture
+        let gesture1 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(gesture: )))
+        
+        let gesture2 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(gesture: )))
+        
+        let gesture3 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(gesture: )))
+        
+        let gesture4 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(gesture: )))
+        
+        let gesture5 = UILongPressGestureRecognizer(target: self, action: #selector(onLongPress(gesture: )))
+        
+        gesture1.minimumPressDuration = 0.1
+        gesture2.minimumPressDuration = 0.1
+        gesture3.minimumPressDuration = 0.1
+        gesture4.minimumPressDuration = 0.1
+        gesture5.minimumPressDuration = 0.1
+        
+        rotateBtn.addGestureRecognizer(gesture1)
+        upBtn.addGestureRecognizer(gesture2)
+        downBtn.addGestureRecognizer(gesture3)
+        leftBtn.addGestureRecognizer(gesture4)
+        rightBtn.addGestureRecognizer(gesture5)
+        
+        
+        
         self.sceneView.autoenablesDefaultLighting = true
         self.sceneView.session.run(configuration)
-              self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
        
+        
+        
         let node = SCNScene(named: "art.scnassets/\(nodeName!).scn")
-        let childNode1 = node?.rootNode.childNode(withName: "\(nodeName!)", recursively: true)
-        childNode1?.position = SCNVector3(-0.5,-1,-2)
-        self.sceneView.scene.rootNode.addChildNode(childNode1!)
+        CurrentNode = node?.rootNode.childNode(withName: "\(nodeName!)", recursively: true)
+        CurrentNode?.position = SCNVector3(-0.5,-1,-2)
+        self.sceneView.scene.rootNode.addChildNode(CurrentNode!)
+        
+        
+        
         
        
     }
@@ -78,6 +120,45 @@ class ArVC: UIViewController {
         }
     }
     
+    
+    @objc func onLongPress(gesture: UILongPressGestureRecognizer){
+        
+        if let ObjNodeEdit = CurrentNode{
+            
+            if gesture.state == .ended {
+                ObjNodeEdit.removeAllActions()
+            }else if gesture.state == .began{
+                if gesture.view === rotateBtn{
+                    let rotate = SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: CGFloat(0.08 * Double.pi), z: 0, duration: 0.1))
+                    ObjNodeEdit.runAction(rotate)
+                    
+                }else if gesture.view === upBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: 0, y: 0.08, z: 0, duration: 0.1))
+                    ObjNodeEdit.runAction(move)
+                }else if gesture.view === downBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: 0, y: -0.08, z: 0, duration: 0.1))
+                    ObjNodeEdit.runAction(move)
+                }else if gesture.view === leftBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: -0.08, y: 0, z: 0, duration: 0.1))
+                    ObjNodeEdit.runAction(move)
+                }else if gesture.view === rightBtn{
+                    let move = SCNAction.repeatForever(SCNAction.moveBy(x: 0.08, y: 0, z: 0, duration: 0.1))
+                    ObjNodeEdit.runAction(move)
+                }
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    @IBAction func scalePressed(_ sender: UIButton) {
+        
+        var scaleFactor = Float(scaleTF.text!) ?? 1.0
+        CurrentNode?.scale = SCNVector3Make(scaleFactor, scaleFactor, scaleFactor)
+        
+    }
     
 }
 
