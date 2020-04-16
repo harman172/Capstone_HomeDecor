864b55ebc781202ef45f472e6d73a82eba65f20e
Parth Dalwadi <pbdalwadi@gmail.com>
2020-04-15 20:29:45 -0400

    object browser VC added

--- Capstone_HomeDecor/Business Account Controllers/AddNewItemViewController.swift
+++ Capstone_HomeDecor/Business Account Controllers/AddNewItemViewController.swift
@@ -10,21 +10,42 @@ import UIKit
 
 class AddNewItemViewController: UIViewController {
 
+    var objDoc: UIDocument?
+    
+    @IBOutlet weak var objPathL: UILabel!
+    
+    @IBOutlet weak var titleL: UITextField!
+    
+    @IBOutlet weak var imageV: UIImageView!
+    
+    @IBOutlet weak var objDescription: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        if let dest = segue.destination as? DocumentBrowserViewController{
+            
+            dest.Delegate_AddObj = self
+            
+        }
+        
+        
+        
     }
-    */
+    
 
 }
